e718643fbd2af9fa7ae400d264737acc2d7af932
renesat <smol.ivan97@gmail.com>
2021-01-30 18:01:37 +0900

    Add helpers and rewrite parsers

--- src/System/Hardware/PL2303Rfid/Cli.hs
+++ src/System/Hardware/PL2303Rfid/Cli.hs
@@ -4,6 +4,10 @@ module System.Hardware.PL2303Rfid.Cli where
 
 import           Options.Applicative
 import qualified Data.ByteString.Char8 as B
+import           Data.List (elemIndex)
+import           Options.Applicative.Types (OptProperties(..))
+import           Options.Applicative.Builder.Internal (optionMod)
+import           Options.Applicative.Help.Pretty (text, (<+>))
 
 import qualified System.Hardware.PL2303Rfid as Core
 
@@ -11,7 +15,6 @@ data CliArgs
   = CliArgs { cliCommand :: CommandArgs
             , cliDevice :: String
             , cliBeep :: Bool
-            -- , color :: Core.LedColor
             }
   deriving (Read, Show, Eq)
 
@@ -20,27 +23,44 @@ data CommandArgs
   | ReadArgs
   | WriteArgs { token     :: B.ByteString
               , lock      :: Core.WriteLock
-              -- , writeType :: Core.Command
+              , writeType :: Core.Command
               }
   deriving (Read, Show, Eq)
 
+-- | Write lock parser.
 writeLockParser :: Parser Core.WriteLock
-writeLockParser = getLock <$> lockSwitch
+writeLockParser = parser desc
   where
-    lockSwitch = switch
-                 ( long "lock"
-                <> short 'l'
-                <> help "Lock after write.")
-    getLock = \x -> if x then
-                      Core.Lock
-                    else
-                      Core.DefultLock
+    desc = ( long "lock"
+          <> short 'l'
+          <> help "Lock after write.")
+    parser = flag Core.DefultLock Core.Lock
+
+-- | Token parser.
+tokenParser :: Parser B.ByteString
+tokenParser = B.pack <$> parser desc
+  where
+    desc = ( metavar "TOKEN"
+          <> help "Token in hex fomat. Length 5 byte.")
+    parser = argument str
+
+writeTypeParser :: Parser Core.Command
+writeTypeParser = enumOption ["write2"    , "write3"   ]
+                             [Core.Write2 , Core.Write3]
+      ( long "type"
+     <> short 't'
+     <> metavar "WRITE_TYPE"
+     <> showDefaultWith (\_ -> "write2")
+     <> value Core.Write2
+     <> help "write type."
+      )
 
 
 writeParser :: Parser CommandArgs
 writeParser = WriteArgs
-  <$> (B.pack <$> argument str (metavar "TOKEN" <> help "Token in hex fomat. Length 5 byte."))
+  <$> tokenParser
   <*> writeLockParser
+  <*> writeTypeParser
 
 commandParser :: Parser CommandArgs
 commandParser = hsubparser
@@ -80,3 +100,26 @@ main = greet =<< execParser opts
     opts = info (cliParser <**> helper)
       ( fullDesc
      <> header "pl2303rfid - cli utils for PL2303 125khz rfid reader/writer" )
+
+-- Helpers
+enumReader :: [String] -> [a] -> String -> ReadM a
+enumReader items values item
+  = case mIndex of
+      Just index -> return $ values !! index
+      Nothing    -> readError
+  where
+    mIndex = elemIndex item items
+    readError = readerAbort $ ErrorMsg  ( "available values: "
+                                       ++ foldl1 (\s x -> s ++ ", " ++ x) items )
+
+enumOption :: [String] -> [a] -> Mod OptionFields a -> Parser a
+enumOption items values desc = option (str >>= enumReader items values)
+                                      (desc <> availableParamMod items)
+
+availableParamMod :: [String] -> Mod f a
+availableParamMod items = optionMod (\x -> x { propHelp = propHelp x >>= (\t -> return $ t <+> text l)
+                                             })
+  where
+    l = ( "(available values: "
+       ++ foldl1 (\s x -> s ++ ", " ++ x) items
+       ++ ")")
